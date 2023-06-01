6eec919c4d81249d68e144fc4af34b4630af0baa
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-06-01 16:30:19 +0100

    4:30PM

--- src/organisation-registration/organisation-registration.controller.ts
+++ src/organisation-registration/organisation-registration.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  Body,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { OrganisationRegistrationService } from './organisation-registration.service';
 import { OrgReg } from './organisation-registration.schema';
 import { CreateOrgRegDto } from './organisation-registration.dto';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 
 @Controller('organisation-registration')
 export class OrganisationRegistrationController {
   constructor(private readonly orgReg: OrganisationRegistrationService) {}
 
   @Get()
-  findAll(): Promise<OrgReg[]> {
-    return this.orgReg.findAll();
+  findAll(@Query() query: ExpressQuery): Promise<OrgReg[]> {
+    return this.orgReg.findAll(query);
   }
+  
 
   @Post()
   async createNewOrg(
@@ -32,7 +42,7 @@ export class OrganisationRegistrationController {
   async deleteOrg(
     @Param('id')
     id: string,
-  ): Promise<OrgReg>{
-    return this.orgReg.deleteOrg(id)
+  ): Promise<OrgReg> {
+    return this.orgReg.deleteOrg(id);
   }
 }
--- src/organisation-registration/organisation-registration.service.ts
+++ src/organisation-registration/organisation-registration.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { OrgReg } from './organisation-registration.schema';
 import * as mongoose from 'mongoose';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 
 @Injectable()
 export class OrganisationRegistrationService {
@@ -9,10 +10,18 @@ export class OrganisationRegistrationService {
     @InjectModel(OrgReg.name) private orgModel: mongoose.Model<OrgReg>,
   ) {}
 
-  async findAll(): Promise<OrgReg[]> {
-    const orgRegs = await this.orgModel.find();
+  async findAll(query: ExpressQuery): Promise<OrgReg[]> {
+    const keyword = query.keyword
+    ? {
+        NameOfBusinessOrganization: {
+            $regex: query.keyword,
+            $options: 'i'
+        }
+    } : {};
+    const orgRegs = await this.orgModel.find({...keyword});
     return orgRegs;
   }
+  
 
   async createNewOrg(newOrg: OrgReg): Promise<OrgReg> {
     const res = await this.orgModel.create(newOrg);
